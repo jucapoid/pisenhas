0609fce4eaf27d14c9a5a06a157c0bbc1e707286
RubenGuedes <32417603+RubenGuedes@users.noreply.github.com>
2017-10-20 19:41:06 +0100

    Add files via upload

---
+++ myapplication/DefinitionActivity.java
@@ -0,0 +1,23 @@
+package com.ruben.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DefinitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_definition);
+    }
+
+    //CRIAR UM CLASSE E ACTIVIVIDADE "CREDITS"    <- ATEEEENÇÃÃÃÃOOOOOOOOOOOOOO
+    /*
+    public void Credits(View view){
+        Intent intent = new Intent (this,CreditsActivity.class);
+        startActivity(intent);
+    }
+    */
+}
