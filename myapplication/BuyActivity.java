7e60c9d61321fa3c0fb3014662c1f7dc381b41d2
RubenGuedes <32417603+RubenGuedes@users.noreply.github.com>
2017-10-24 23:42:34 +0100

    Add files via upload

---
+++ myapplication/BuyActivity.java
@@ -0,0 +1,35 @@
+package com.ruben.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.database.sqlite.*;
+
+import java.io.File;
+
+public class BuyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy);
+    }
+
+
+
+    // Receberá uma tabela(File ou Base de Dados) com as refeções para a semana.
+
+
+    // Criar algo para selecionar o dia.
+
+
+    /*Recebendo o dia o programa devolverá uma tabela
+    com os vários tipos de senhas possíveis */
+
+
+    /* Ao selecionar a senha, pode-se ver os
+    detalhes(quais as refeições) e ,na página, é exibido um botão para comprar a senha
+
+    -> Será guardado num array ou no SQL ?? */
+
+
+}
